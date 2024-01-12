0da3a9e64e6c42c3bee0e7f29e573ba8b5586610
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-12 22:34:13 +0200

    lockboxes update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
 You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 
+Each box is numbered sequentially from 0 to n - 1
 and each box may contain keys to the other boxes.
 """
 from typing import List
+
+
 def canUnlockAll(boxes: List[List[int]]) -> bool:
     """method that determines if all the boxes can be opened"""
     unlocked = set()
+
     def search(box):
         """ add unlocked boxes"""
         unlocked.add(box)
