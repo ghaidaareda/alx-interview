43e9cf5692887bdf53b087af7927620d4b33bda7
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-18 23:37:04 +0200

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+get numb of operations
+"""
+
+
+def minOperations(n):
+    """ min number of operation"""
+    if n < 2:
+        return 0  # It's not possible to achieve n if n is less than 2
+
+    operations = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    return operations
