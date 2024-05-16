4234e5140625099392da386fbb2954519e9aad69
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-16 23:35:41 +0300

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,11 +20,11 @@ def main():
         for line in sys.stdin:
             line_count += 1
             parts = line.split()
-            if len(parts) != 7:
+            if len(parts) != 10:
                 continue
-            ip, _, _, status_code, size = parts
-            if status_code.isdigit():
-                total_size += int(size)
+            status_code = parts[8]
+            if status_code.isdigit() and int(status_code) in [200, 301, 400, 401, 403, 404, 405, 500]:
+                total_size += int(parts[9])
                 status_counts[status_code] = status_counts.get(status_code, 0) + 1
             if line_count % 10 == 0:
                 print_stats(total_size, status_counts)
