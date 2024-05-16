6ba357e6cb9d6284702fd4e2286cd3988b0b47ef
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-16 23:27:15 +0300

    q

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+
 def is_safe(board, row, col):
     for i in range(row):
         if board[i] == col or \
-            board[i] - i == col - row or \
-            board[i] + i == col + row:
+                board[i] - i == col - row or \
+                board[i] + i == col + row:
             return False
     return True
+
 
 def solve_nqueens(board, row, N):
     if row == N:
@@ -18,9 +20,11 @@ def solve_nqueens(board, row, N):
             board[row] = col
             solve_nqueens(board, row + 1, N)
 
+
 def print_solution(board):
     solution = [[i, board[i]] for i in range(len(board))]
     print(solution)
+
 
 def nqueens(N):
     if not N.isdigit():
@@ -31,6 +35,7 @@ def nqueens(N):
         print("N must be at least 4")
         sys.exit(1)
     solve_nqueens([-1] * N, 0, N)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
