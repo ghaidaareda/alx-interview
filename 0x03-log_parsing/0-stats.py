3d094f1d80bee45453ae3aa158115b8b3da63a3c
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-16 23:32:27 +0300

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
 """
-Log parsing
+Log parsing (interview task)
 """
 
 import sys
 
-if __name__ == '__main__':
+def print_stats(total_size, status_counts):
+    print("File size: {}".format(total_size))
+    sorted_status = sorted(status_counts.items(), key=lambda x: int(x[0]))
+    for code, count in sorted_status:
+        print("{}: {}".format(code, count))
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
-
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+def main():
+    total_size = 0
+    status_counts = {}
 
     try:
+        line_count = 0
         for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
+            line_count += 1
+            parts = line.split()
+            if len(parts) != 7:
+                continue
+            ip, _, _, status_code, size = parts
+            if status_code.isdigit():
+                total_size += int(size)
+                status_counts[status_code] = status_counts.get(status_code, 0) + 1
+            if line_count % 10 == 0:
+                print_stats(total_size, status_counts)
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+        print_stats(total_size, status_counts)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
