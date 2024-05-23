9143f45167378af79738450a1035adc67097d6f0
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-24 01:34:04 +0300

    starwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
-const axios = require('axios');
-
-async function getMovieCharacters(movieId) {
-    try {
-        const response = await axios.get(`https://swapi.dev/api/films/${movieId}/`);
-        const movieData = response.data;
-        const charactersUrls = movieData.characters;
-
-        for (const characterUrl of charactersUrls) {
-            const characterResponse = await axios.get(characterUrl);
-            const characterData = characterResponse.data;
-            console.log(characterData.name);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error.message);
-    }
-}
-
+const request = require('request');
 const movieId = process.argv[2];
-if (!movieId) {
-    console.error('Please provide a movie ID as a command line argument.');
-} else {
-    getMovieCharacters(movieId);
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET',
+};
+
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    getMoviecharaters(characters, 0);
+  }
+});
+
+function getMoviecharaters(characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        getMoviecharaters(characters, index + 1);
+      }
+    }
+  });
 }
